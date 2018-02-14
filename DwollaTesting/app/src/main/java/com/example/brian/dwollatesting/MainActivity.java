1194eac1df676d455b3cb9e22c71148f425e2771
briancap <btc5081@gmail.com>
2018-02-13 19:39:25 -0500

    add Dwolla lib and dependencies

--- DwollaTesting/app/src/main/java/com/example/brian/dwollatesting/MainActivity.java
+++ DwollaTesting/app/src/main/java/com/example/brian/dwollatesting/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.brian.dwollatesting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.dwolla.java.sdk.requests.SendRequest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
